3144995697d1415259ad0abc41e6576f3e55ad69
Anastasiya <kisaost777@gmail.com>
2020-01-10 00:57:30 +0500

    Выполнила задачу

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QStyleFactory
-from PyQt5 import uic
 import random
 from PyQt5.QtGui import QPainter, QColor
 import sys
+from ui import Ui_MainWindow
 
 
-class Form(QMainWindow):
+class Form(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.s_draw)
         self.is_draw = False
 
@@ -24,9 +24,9 @@ class Form(QMainWindow):
 
     def draw(self, qp):
         if self.is_draw:
-            qp.setBrush(QColor('yellow'))
             for i in range(25):
                 rad = random.randint(0, 200)
+                qp.setBrush(QColor(random.choice(['yellow', 'red', 'cyan', 'gray', 'black'])))
                 qp.drawEllipse(random.randint(0, 420), random.randint(0, 410), rad, rad)
             self.is_draw = False
 
@@ -35,6 +35,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     QApplication.setStyle(QStyleFactory.create('Fusion'))
     form = Form()
-    form.setFixedSize(440, 410)
     form.show()
     sys.exit(app.exec())
---
+++ ui.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'UI.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(438, 407)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 320, 421, 81))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Git и случайные окружности"))
+        self.pushButton.setText(_translate("MainWindow", "Случайные окружности"))
